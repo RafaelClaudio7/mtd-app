6de841a2e736393619d82ff3ee3a74e5d3fdcb76
RafaelClaudio7 <rc7true@gmail.com>
2022-12-29 11:00:50 -0300

    feat: estilo do card de task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Flex, Heading, Input } from "@chakra-ui/react";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { Avatar, Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Flex, Heading, Input, Stack, Image, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -26,6 +27,25 @@ export default function Home() {
           </Box>
           <Avatar name="Rafael Tula" mr="20px" bg="teal.500" />
         </Heading>
+        <Flex>
+          <Card maxW="sm" bg="#323232" ml="40px">
+            <CardBody>
+              <Stack mt="6" spacing="3">
+                <Heading size="sm" color="white" textAlign="center">Realizar protótipo da interface no figma</Heading>
+                <Text textAlign="center" color="#888888">
+                Criar telas de login e cadastro, e também a página home da aplicação
+                </Text>
+                <Text color="red.600" fontSize="sm">
+                  To do
+                </Text>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+            <EditIcon w={5} h={5} color="#888888" />
+            <DeleteIcon w={5} h={5} color="#888888" />
+            </CardFooter>
+          </Card>
+        </Flex>
       </Flex>
     </Flex>
   );
